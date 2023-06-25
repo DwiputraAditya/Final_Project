bf36007c15d972fa222e590c1d2afaac7abc31cb
DwiputraAditya <you@example.com>
2023-06-25 14:23:13 +0700

    Final API

--- src/main/java/org/binar/kamihikoukiairlines/controller/InvoiceController.java
+++ src/main/java/org/binar/kamihikoukiairlines/controller/InvoiceController.java
@@ -1,6 +1,8 @@
 package org.binar.kamihikoukiairlines.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.binar.kamihikoukiairlines.service.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/invoice")
@@ -20,9 +23,30 @@ public class InvoiceController {
 
     @Autowired
     InvoiceService invoiceService;
+    @Autowired
+    private HttpServletResponse response;
 
-    @GetMapping("/generateTicket/{bookingId}")
-    public ResponseEntity<?> printTicket(@PathVariable Long bookingId) {
+    @GetMapping("/generateTicket")
+    public void getInvoiceReport(Long bookingId) throws Exception{
+        response.setContentType("application/pdf");
+        response.setHeader("Content-disposition", "attachment; filename=\"tiket.pdf\"");
+        JasperPrint jasperPrint = invoiceService.generateInvoice(bookingId);
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+    }
+
+    /*public ResponseEntity<byte[]> generateTicket(@PathVariable Long bookingId) {
+        try {
+            byte[] ticketBytes = invoiceService.generateTicket(bookingId);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("attachment", "ticket.pdf");
+            return new ResponseEntity<>(ticketBytes, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }*/
+
+    /*public ResponseEntity<?> printTicket(@PathVariable Long bookingId) {
         try {
             byte[] pdfBytes = invoiceService.generateTicket(bookingId);
             ByteArrayResource resource = new ByteArrayResource(pdfBytes);
@@ -37,5 +61,5 @@ public class InvoiceController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-    }
+    }*/
 }
--- src/main/java/org/binar/kamihikoukiairlines/service/InvoiceService.java
+++ src/main/java/org/binar/kamihikoukiairlines/service/InvoiceService.java
@@ -3,13 +3,19 @@ package org.binar.kamihikoukiairlines.service;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
 import org.binar.kamihikoukiairlines.model.Booking;
 import org.binar.kamihikoukiairlines.model.Passenger;
 import org.binar.kamihikoukiairlines.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,13 +24,34 @@ import java.util.Map;
 @Slf4j
 public class InvoiceService {
     @Autowired
+    private DataSource dataSource;
+    @Autowired
     BookingRepository bookingRepository;
 
-    public byte[] generateTicket(Long bookingId) throws Exception {
+    public Connection getConnection(){
+        try {
+            return dataSource.getConnection();
+        }catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public JasperPrint generateInvoice(Long bookingId) throws Exception {
+        InputStream fileReport = new ClassPathResource("ticket/TiketPesawat.jrxml").getInputStream();
+        JasperReport jasperReport = JasperCompileManager.compileReport(fileReport);
+        Map<String, Object> params = new HashMap<>();
+        params.put("bookingId", bookingId);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, getConnection());
+        log.info("Successfully generated ticket for booking id: " + bookingId);
+        return jasperPrint;
+    }
+
+    /*public byte[] generateTicket(Long bookingId) throws Exception {
         Booking booking = bookingRepository.findById(bookingId)
                 .orElseThrow(() -> new Exception("Booking not found"));
 
-        InputStream reportTemplate = getClass().getResourceAsStream("/ticket/kamihikouki.jrxml");
+        InputStream reportTemplate = getClass().getResourceAsStream("/ticket/TiketPesawat.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(reportTemplate);
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("bookingId", bookingId);
@@ -35,5 +62,36 @@ public class InvoiceService {
 
         log.info("Has successfully  generate ticket for booking id: " + bookingId);
         return pdfBytes;
-    }
+    }*/
+
+
+    /*public JasperPrint generateInvoice(Long bookingId) throws Exception{
+        InputStream fileReport = new ClassPathResource("ticket/TiketPesawat.jasper").getInputStream();
+        JasperReport jasperReport = (JasperReport) JRLoader.loadObject(fileReport);
+        Map<String, Object> params = new HashMap<>();
+        params.put("bookingId", bookingId);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, getConnection());
+        return jasperPrint;
+    }*/
+
+    /*public JasperPrint generateTicket(Long bookingId) throws Exception {
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new Exception("Booking not found"));
+
+        InputStream reportTemplate = getClass().getResourceAsStream("/ticket/kamihikouki.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportTemplate);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("bookingId", bookingId);
+        List<Passenger> passengers = booking.getPassengersList();
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(passengers);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+        byte[] pdfBytes = outputStream.toByteArray();
+
+        log.info("Successfully generated ticket for booking id: " + bookingId);
+        return pdfBytes;
+    }*/
+
 }
